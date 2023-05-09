2d6085571253f723d224f24b3828c9283b0d8de2
Sona Punuru <punuru@email.unc.edu>
2023-05-09 17:14:16 -0400

    changed game

--- control/control.js
+++ control/control.js
@@ -1,84 +1,86 @@
-export { rps, rpsls }
-
-
-//create rps function
-function rps(shot) {
-	let computer = Math.floor(Math.random()*3);
-	computer = computer == 0 ? "rock" :
-					computer == 1 ? "paper" :
-					"scissors";
-	if (shot == undefined) {
-		return { player: computer };
-	}
-	let user = shot.toLowerCase();
-
-	let options = ['rock', 'paper', 'scissor'];
-
-	if (!options.includes(user)) {
-			if (user === "lizard" || user === "spock") {
-				console.error(`${shot} is out of range.`);
-				throw new RangeError();
-			} else {
-				console.error(`${shot} is not a valid choice.`);
-				throw new RangeError();
-			}
-		}
-
-	let result = '';
-
-	if (computer === user) {
-		result = "tie";
-	} else if (computer === "rock" && user === "scissors" || computer === "paper" && user === "rock" || computer === "scissors" && user === "paper") {
-		result = "lose";
-	} else {
-		result = "win";
-	}
-	
-        return { player: user,
-		opponent: computer,
-		result: result
-	};
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
 
+export function rps(player_input) {
 
-//create rpsls function
-function rpsls(shot) {
-	let computer = Math.floor(Math.random()*5);
-	computer = computer == 0 ? "rock" :
-		computer == 1 ? "paper" :
-		computer == 2 ? "scissors" :
-		computer == 3 ? "lizard" :
-		"spock";
-
-	if (shot == undefined) {
-		return { player: computer };
-	}
-
-	let options = ['rock', 'paper', 'scissor', 'lizard', 'spock'];
-
-	let user = shot.toLowerCase();
-
-	if (!options.includes(user)) {
-		console.error(`${shot} is not a valid choice.`);
-		throw new RangeError();
-	}
-	let result = '';
-
-	if (computer === user) {
-		result = "tie";
-	} else if (computer === "rock" && user === "scissors" || computer === "paper" && user === "rock" || computer === "scissors" && user === "paper") {
-		result = "lose";
-	} else if (computer === "rock" && user === "lizard" || computer === "lizard" && user === "spock" || computer === "spock" && user === "rock") {
-		result = "lose";
-	} else if (computer === "spock" && user === "scissors" || computer === "scissors" && user === "lizard" || computer === "paper" && user === "spock" || computer === "lizard" && user === "paper") {
-		result = "lose";
-	} else {
-		result = "win";
-	}
-
-	return { player: user,
-		opponent: computer,
-		result: result
-	};
+  let valid = ["rock", "paper", "scissors"];
+  let opponent = valid[getRandomInt(3)];
+  
+  if (player_input == undefined) {
+        let result = '{"player":' + '"' + opponent + '"' + '}';
+        return result;
+  }
+  
+  else {
+        let result = '{"player":' + '"' + player_input + '",' + '"opponent":' + '"' + opponent + '",' + '"result":' + '"' + win_loss(player_input, opponent) + '"}';
+        return result;
+      
+  }
 }
 
+export function rpsls(player_input) {
+
+  let valid = ["rock", "paper", "scissors", "lizard", "spock"];
+  let opponent = valid[getRandomInt(5)];
+  
+  if (player_input == undefined) {
+        let result = '{"player":' + '"' + opponent + '"' + '}';
+        return result;
+  }
+  else if (valid.includes(player_input)) {
+        let result = '{"player":' + '"' + player_input + '",' + '"opponent":' + '"' + opponent + '",' + '"result":' + '"' + win_loss(player_input, opponent) + '"}';
+        return result;
+      
+  }
+}
+
+function win_loss(player_input, opponent_input) {
+  if (player_input == "rock") {
+      if (opponent_input == "paper" || opponent_input == "spock") {
+          return "lose!!!!!! lol";
+      }
+      else if (opponent_input == "lizard" || opponent_input == "scissors") {
+          return "win!!!!!!! yayyayyayay";
+      }
+  }
+  
+  if (player_input == "paper") {
+      if (opponent_input == "scissors" || opponent_input == "lizard") {
+          return "loserr";
+      }
+      else if (opponent_input == "rock" || opponent_input == "spock") {
+          return "winnerrrrr";
+      }
+  }
+  
+  if (player_input == "scissors") {
+      if (opponent_input == "rock" || opponent_input == "spock") {
+          return "lose hahahah";
+      }
+      else if (opponent_input == "paper" || opponent_input == "lizard") {
+          return "win ok ok ok ";
+      }
+  }
+  
+  if (player_input == "lizard") {
+      if (opponent_input == "rock" || opponent_input == "scissors") {
+          return "lose lol!!!";
+      }
+      else if (opponent_input == "spock" || opponent_input == "paper") {
+          return "win congrats buddy";
+      }
+  }
+  
+  if (player_input == "spock") {
+      if (opponent_input == "paper" || opponent_input == "lizard") {
+          return "lose !!! try harder ";
+      }
+      else if (opponent_input == "rock" || opponent_input == "scissors") {
+          return "winner winner chicken dinner";
+      }
+  }
+  
+  return "tieeeeeee";
+
+}
